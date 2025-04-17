aad8beb4f67fe9d32ffd2959f8fccad6e4df1133
Triplehash <168893404+TriplehashRanchi@users.noreply.github.com>
2025-04-17 16:09:20 +0530

    Update page.tsx

--- app/(defaults)/page.tsx
+++ app/(defaults)/page.tsx
@@ -11,7 +11,7 @@ const Sales = () => {
     return (
         <div>
             <ComponentsDashboardFinance />
-            <ComponentsDashboardAnalytics />
+            {/* <ComponentsDashboardAnalytics /> */}
         </div>
     );
 };
