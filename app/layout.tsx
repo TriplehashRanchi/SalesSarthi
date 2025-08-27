0f9133f85fb786b99e1c5ac5b288ed830584e3ee
Triplehash <168893404+TriplehashRanchi@users.noreply.github.com>
2025-08-27 16:55:49 +0530

    tired

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import '../styles/tailwind.css';
 import { Metadata } from 'next';
 import { Nunito, Righteous } from 'next/font/google';
 import Head from 'next/head';
-import SafeArea from '@/components/SafeArea';
 
 export const metadata: Metadata = {
     title: {
@@ -31,9 +30,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="en">
             <Head> <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" /></Head>
             <body className={`${nunito.variable} ${righteous.variable}`}>
-                <SafeArea edges={['top','bottom']} className="min-h-screen flex flex-col">
                 <ProviderComponent>{children}</ProviderComponent>
-                </SafeArea>
             </body>
         </html>
     );
