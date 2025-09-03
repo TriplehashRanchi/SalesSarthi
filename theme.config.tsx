e9785cd7edad83cb5de8e3708ce171f6888d6cb4
Triplehash <168893404+TriplehashRanchi@users.noreply.github.com>
2025-09-04 00:17:01 +0530

    update code

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ const themeConfig = {
     locale: 'en', // en, da, de, el, es, fr, hu, it, ja, pl, pt, ru, sv, tr, zh
     theme: 'light', // light, dark, system
     menu: 'vertical', // vertical, collapsible-vertical, horizontal
-    layout: 'full', // full, boxed-layout
+    layout: 'boxed-layout', // full, boxed-layout
     rtlClass: 'ltr', // rtl, ltr
     animation: '', // animate__fadeIn, animate__fadeInDown, animate__fadeInUp, animate__fadeInLeft, animate__fadeInRight, animate__slideInDown, animate__slideInLeft, animate__slideInRight, animate__zoomIn
     navbar: 'navbar-sticky', // navbar-sticky, navbar-floating, navbar-static
