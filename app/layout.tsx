35368ffe35ad8a829c073b04194692eea2cf6cca
Triplehash <168893404+TriplehashRanchi@users.noreply.github.com>
2025-09-03 20:07:21 +0530

    www

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
             <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+                <meta
+                name="viewport"
+                content="viewport-fit=cover, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+                />
             </Head>
             <body
                 className={`${nunito.variable} ${righteous.variable} mt-10 md:mt-0`}
